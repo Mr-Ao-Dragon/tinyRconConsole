ecc40029ed64cb7a765b814cde7cb8432735d824
敖律风 <x123456789fy@outlook.com>
2024-09-30 08:38:09 +0800

    update: add setting init

--- go.mod
+++ go.mod
@@ -3,22 +3,22 @@ module github.com/Mr-Ao-Dragon/tinyRconConsole
 go 1.23.0
 
 require (
+	github.com/GmSSL/GmSSL-Go v1.3.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/nutsdb/nutsdb v1.0.4
 	github.com/rs/zerolog v1.33.0
 )
 
 require (
-	github.com/antlabs/stl v0.0.1 // indirect
+	github.com/antlabs/stl v0.0.2 // indirect
 	github.com/antlabs/timer v0.0.11 // indirect
 	github.com/bwmarrin/snowflake v0.3.0 // indirect
-	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/gofrs/flock v0.12.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/tidwall/btree v1.6.0 // indirect
+	github.com/tidwall/btree v1.7.0 // indirect
 	github.com/xujiajun/mmap-go v1.0.1 // indirect
 	github.com/xujiajun/utils v0.0.0-20220904132955-5f7c5b914235 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
--- storage/init.go
+++ storage/init.go
@@ -1,6 +1,11 @@
 package storage
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/binary"
+	"errors"
 	"github.com/mitchellh/go-homedir"
 	"github.com/nutsdb/nutsdb"
 	"github.com/rs/zerolog"
@@ -20,7 +25,10 @@ func CreateConfigDirectory() (configDir string, err error) {
 	}
 	configDir = homePath + "/.config/tinyRconConsole/storage"
 	_, err = os.Stat(configDir)
-	if err != nil {
+	if os.IsExist(err) {
+		return configDir, errors.New("config directory already exists")
+	}
+	if os.IsNotExist(err) {
 		err = os.MkdirAll(configDir, 0744)
 	}
 	if err != nil {
@@ -28,14 +36,81 @@ func CreateConfigDirectory() (configDir string, err error) {
 	}
 	return configDir, nil
 }
-func initDatabase(configDir string) (err error) {
+func initDatabase(DBPath string) (err error) {
 	db, err := nutsdb.Open(
 		nutsdb.DefaultOptions,
-		nutsdb.WithDir(configDir),
+		nutsdb.WithDir(DBPath),
 	)
-	defer db.Close()
+	defer func(db *nutsdb.DB) {
+		_ = db.Close()
+	}(db)
 	if err != nil {
 		return err
+	}
+	buckets := make([]func(tx *nutsdb.Tx) error, 0)
+	buckets = append(buckets, func(tx *nutsdb.Tx) error {
+		return tx.NewBucket(nutsdb.DataStructureSet, "settings")
+	})
+	buckets = append(buckets, func(tx *nutsdb.Tx) error {
+		return tx.NewBucket(nutsdb.DataStructureList, "servers")
+	})
+	buckets = append(buckets, func(tx *nutsdb.Tx) error {
+		return tx.NewBucket(nutsdb.DataStructureBTree, "savedCommand")
+	})
+	for _, v := range buckets {
+		if err = db.Update(v); err != nil {
+			return errors.Join(err, errors.New("fail to create bucket"))
+		}
+	}
+	tx, err := db.Begin(true)
+	if err != nil {
+		return errors.Join(err, errors.New("fail to begin tx"))
+	}
+	var binData []byte
+	_, _ = binary.Encode(binData, binary.NativeEndian, "en-US")
+	err = tx.Put("settings", []byte("language"), binData, 0)
+	if err != nil {
+		_ = tx.Rollback()
+		return errors.Join(err, errors.New("fail to put"))
+	}
+	_, _ = binary.Encode(binData, binary.NativeEndian, "buttIn")
+	err = tx.Put("settings", []byte("storageBackend"), binData, 0)
+	if err != nil {
+		_ = tx.Rollback()
+		return errors.Join(err, errors.New("fail to put"))
+	}
+	_, _ = binary.Encode(binData, binary.NativeEndian, "SM2")
+	err = tx.Put("settings", []byte("cryptoBackend"), binData, 0)
+	if err != nil {
+		_ = tx.Rollback()
+		return errors.Join(err, errors.New("fail to put"))
+	}
+	_, _ = binary.Encode(binData, binary.NativeEndian, true)
+	err = tx.Put("settings", []byte("tcpKeepLive"), binData, 0)
+	if err != nil {
+		_ = tx.Rollback()
+		return errors.Join(err, errors.New("fail to put"))
+	}
+	_, _ = binary.Encode(binData, binary.NativeEndian, time.Now().In(time.Now().Location()))
+	err = tx.Put("settings", []byte("timezone"), binData, 0)
+	if err != nil {
+		_ = tx.Rollback()
+		return errors.Join(err, errors.New("fail to put"))
+	}
+	privateKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return errors.Join(err, errors.New("fail to generate key"))
+	}
+	_, _ = binary.Encode(binData, binary.NativeEndian, privateKey)
+	err = tx.Put("settings", []byte("privateKey"), binData, 0)
+	if err != nil {
+		_ = tx.Rollback()
+		return errors.Join(err, errors.New("fail to put"))
+	}
+	err = tx.Commit()
+	if err != nil {
+		_ = tx.Rollback()
+		return errors.Join(err, errors.New("fail to commit setup data"))
 	}
 	return nil
 }
@@ -45,7 +120,7 @@ func init() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	configDir, err := CreateConfigDirectory()
-	if err != nil {
+	if !errors.Is(err, errors.New("config directory already exists")) || err != nil {
 		log.Fatal().AnErr("error: ", err).Msgf("fail to create dir")
 	}
 	err = initDatabase(configDir)
